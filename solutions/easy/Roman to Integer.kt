963cbe30572a1ba3e1f2ac48a9957e2107a339b2
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-08-08 19:41:04 +0500

    Update Roman to Integer.kt

--- solutions/easy/Roman to Integer.kt
+++ solutions/easy/Roman to Integer.kt
@@ -3,15 +3,29 @@
             // Language: Kotlin
             // Link: https://leetcode.com/problems/roman-to-integer/
 
-            val current = romanNumbers[it]!!
-            
-            if (current < prev){
-                result -= current
-            }
-            else result += current
-            
-            prev = current
-        }
-        return result
-    }
-}
+           class Solution {
+                fun romanToInt(s: String): Int {
+                    val romanNumbers = mapOf<Char,Int>(
+                        'I' to 1, 
+                        'V' to 5,
+                        'X' to 10,
+                        'L' to 50,
+                        'C' to 100,
+                        'D' to 500,
+                        'M' to 1000
+                    ) 
+                    var prev = 0
+                    var result = 0
+                    for(it in s.length - 1 downTo 0){
+                        val current = romanNumbers[s[it]]!!
+                        
+                        if (current < prev){
+                            result -= current
+                        }
+                        else result += current
+                        
+                        prev = current
+                    }
+                    return result
+                }
+            }
