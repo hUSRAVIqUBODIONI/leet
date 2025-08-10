8b5ca7026dc28b541f0aa07ab9b9333b15e4450a
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-08-10 17:10:43 +0500

    Add solution for Remove Duplicates from Sorted List

---
+++ solutions/easy/Remove Duplicates from Sorted List.kt
@@ -0,0 +1,21 @@
+// Title: Remove Duplicates from Sorted List
+            // Difficulty: Easy
+            // Language: Kotlin
+            // Link: https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+        var curr = head.next
+        while(curr != null){
+            if(curr?.`val` == prev?.`val`){
+        var prev = head
+        dummy.next = head
+        val dummy =ListNode(-1)
+        
+        if(head == null || head.next == null) return head
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+class Solution {
+ */
+ * }
+ *     var next: ListNode? = null
+ * class ListNode(var `val`: Int) {
+ * Definition for singly-linked list.
+ * var v = li.`val`
