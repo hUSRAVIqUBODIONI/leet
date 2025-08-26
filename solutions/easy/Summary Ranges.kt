259f60c2ad0ce3494223706e507843ddb3855599
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-08-27 01:20:25 +0500

    Add solution for Summary Ranges

---
+++ solutions/easy/Summary Ranges.kt
@@ -0,0 +1,21 @@
+// Title: Summary Ranges
+            // Difficulty: Easy
+            // Language: Kotlin
+            // Link: https://leetcode.com/problems/summary-ranges/
+
+            while(i != n -1 && nums[i] + 1 == nums[i+1]){
+                i+=1
+            }
+                end = nums[i+1]
+
+
+            var end = nums[i]
+            val start = nums[i]
+        while( i < n ){
+        var i = 0
+
+        val result = mutableListOf<String>()
+        val n = nums.size
+            if( start == end) result.add("$start") else result.add("$start->$end")
+        } 
+            i+=1
