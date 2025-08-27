85b17e69f63321d48cc5384b1e72c23cb374aa5d
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-08-27 14:02:33 +0500

    Add solution for String Compression

---
+++ solutions/medium/String Compression.kt
@@ -0,0 +1,21 @@
+// Title: String Compression
+            // Difficulty: Medium
+            // Language: Kotlin
+            // Link: https://leetcode.com/problems/string-compression/
+
+            }
+            chars[w] = prev_char
+            if(count > 1){
+                for(c in count.toString()){
+            }
+                    chars[w] = c
+                }
+                r +=1
+                count+=1
+            while(r < n  && prev_char == chars[r]){
+            val prev_char = chars[r]
+        }
+            var count = 0
+        while( r < n ){
+            w++
+                    w++
