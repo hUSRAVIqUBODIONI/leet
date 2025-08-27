b2d720eb13184e3d2403505a0a5ec97c41c5676d
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-08-27 21:42:11 +0500

    Add solution for Move Zeroes

---
+++ solutions/easy/Move Zeroes.kt
@@ -0,0 +1,20 @@
+// Title: Move Zeroes
+            // Difficulty: Easy
+            // Language: Kotlin
+            // Link: https://leetcode.com/problems/move-zeroes/
+
+            if(nums[r] != 0){
+        }
+                val temp = nums[l]
+            }
+                nums[l] = nums[r]
+                nums[r] = temp
+                l++
+        for(r in 0 until n){
+        var l = 0
+        var r = 0
+
+        
+        val n = nums.size 
+    fun moveZeroes(nums: IntArray): Unit {
+class Solution {
