ffc2b224343647998828c33745f8e923174d5227
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-08-27 15:26:40 +0500

    Add solution for Valid Palindrome

---
+++ solutions/easy/Valid Palindrome.kt
@@ -0,0 +1,21 @@
+// Title: Valid Palindrome
+            // Difficulty: Easy
+            // Language: Kotlin
+            // Link: https://leetcode.com/problems/valid-palindrome/
+
+
+        while(l<=r){
+            if(!s[l].isLetterOrDigit()) { 
+                l++ 
+            }
+            if(!s[r].isLetterOrDigit()) {
+                r--
+            }
+                continue 
+                continue
+        var r = s.length - 1
+        var l = 0
+            if( s[l].lowercaseChar() != s[r].lowercaseChar()) return false
+    fun isPalindrome(s: String): Boolean {
+class Solution {
+            l++
