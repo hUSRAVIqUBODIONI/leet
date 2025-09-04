66f58c84a40446118fed1d4d94e4748e463840e7
Шоимов Шоди <109214097+hUSRAVIqUBODIONI@users.noreply.github.com>
2025-09-05 01:53:27 +0500

    Add solution for Binary Tree Inorder Traversal

---
+++ solutions/easy/Binary Tree Inorder Traversal.kt
@@ -0,0 +1,24 @@
+// Title: Binary Tree Inorder Traversal
+            // Difficulty: Easy
+            // Language: Kotlin
+            // Link: https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+        }
+            if(node == null) return 
+
+            dfs(node.left)
+            result.add(node.`val`)
+            dfs(node.right)
+
+
+        dfs(root)
+        return result
+
+        fun dfs(node: TreeNode?) {
+
+        val result = mutableListOf<Int>()
+
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+class Solution {
+ */
+ * }
